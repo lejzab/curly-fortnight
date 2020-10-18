b2f6bcf5c413f301d6aac3f6f6f11e58ee614698
Paweł <pawel.bogaczewicz@outlook.com>
2020-10-18 19:25:02 +0200

    prime factors. counting factors

--- problems/003/pfactor.go
+++ problems/003/pfactor.go
@@ -28,23 +28,27 @@ func main() {
 
 	log.Infof("The prime factors of %d are:\n", number)
 	for _, v := range pf1 {
-		log.Infof("\t%d", v)
+		log.Infof("\t%d. Number of this factor: %d", v, pf[v])
 	}
 	log.Infof("The largest prime factor of %d is: %d", number, pf1[len(pf1)-1])
 	log.Info("STOP")
 }
 
-func PrimeFator(n int) map[int]bool {
-	pf := map[int]bool{}
+func PrimeFator(n int) map[int]int {
+	pf := map[int]int{}
 	lim := int(math.Sqrt(float64(n))) + 1
-	if n%2 == 0 {
-		n = n / 2
-		pf[2] = true
+	for {
+		if n%2 == 0 {
+			n = n / 2
+			pf[2]++
+		} else {
+			break
+		}
 	}
 	for i := 3; i < lim && i <= n; i += 2 {
 		if n%i == 0 {
 			n = n / i
-			pf[i] = true
+			pf[i]++
 		}
 	}
 
