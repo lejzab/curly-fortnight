9cf9c5bee8414db25d2fa8ed4583fe4c6f098dc1
Paweł <pawel.bogaczewicz@outlook.com>
2020-10-18 19:17:55 +0200

    prime factors

--- problems/003/pfactor.go
+++ problems/003/pfactor.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	log "github.com/sirupsen/logrus"
+	"math"
+	"sort"
 )
 
 const taskDescr = `The prime factors of 13195 are 5, 7, 13 and 29.
@@ -17,10 +19,34 @@ func main() {
 	var number int
 	flag.IntVar(&number, "number", 13195, "number to factorize")
 	flag.Parse()
+	pf := PrimeFator(number)
+	pf1 := []int{}
+	for v := range pf {
+		pf1 = append(pf1, v)
+	}
+	sort.Ints(pf1)
+
+	log.Infof("The prime factors of %d are:\n", number)
+	for _, v := range pf1 {
+		log.Infof("\t%d", v)
+	}
+	log.Infof("The largest prime factor of %d is: %d", number, pf1[len(pf1)-1])
 	log.Info("STOP")
 }
 
 func PrimeFator(n int) map[int]bool {
 	pf := map[int]bool{}
+	lim := int(math.Sqrt(float64(n))) + 1
+	if n%2 == 0 {
+		n = n / 2
+		pf[2] = true
+	}
+	for i := 3; i < lim && i <= n; i += 2 {
+		if n%i == 0 {
+			n = n / i
+			pf[i] = true
+		}
+	}
+
 	return pf
 }
