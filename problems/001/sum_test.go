c02165b7f0b2000870d84cd9837a72d816544d99
Paweł <pawel.bogaczewicz@outlook.com>
2020-10-18 18:10:04 +0200

    sum of fibb terms

--- problems/001/sum_test.go
+++ problems/001/sum_test.go
@@ -5,13 +5,13 @@ import "testing"
 func TestSimpleSum(t *testing.T) {
 	total := SimpleSum(10)
 	if total != 23 {
-		t.Errorf("SimpleSum was incorrect, got: %d, want: %d.", total, 23)
+		t.Errorf("SimpleSum was incorrect, want: %d, got: %d.", 23, total)
 	}
 }
 
 func TestArithmeticSum(t *testing.T) {
 	total := ArithmeticSum(10)
 	if total != 23 {
-		t.Errorf("SimpleSum was incorrect, got: %d, want: %d.", total, 23)
+		t.Errorf("SimpleSum was incorrect, want: %d, got: %d.", 23, total)
 	}
 }
